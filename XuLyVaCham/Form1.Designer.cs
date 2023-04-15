60d42b0e62da238e3ed7eaeec86b327949e5fcf2
ThuanNQT <thuan13112003@gmail.com>
2023-04-15 21:44:21 +0700

    thuanNqt

---
+++ XuLyVaCham/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace XuLyVaCham
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gold;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "object";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Gold;
+            this.pictureBox2.Location = new System.Drawing.Point(677, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "object";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Gold;
+            this.pictureBox3.Location = new System.Drawing.Point(677, 334);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "object";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Gold;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 334);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "object";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Green;
+            this.player.Location = new System.Drawing.Point(343, 334);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(120, 120);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(342, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ThuanJJ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "C#";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+    }
+}
---
+++ XuLyVaCham/Form1.cs
@@ -0,0 +1,94 @@
+﻿namespace XuLyVaCham
+{
+    public partial class Form1 : Form
+    {
+        bool moveLeft, moveRight,moveUp,moveDown;//dừng để check xem người dùng nhập phím nào
+        int speed = 12;//tốc độ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //xử lý sự kiện cho các phím 
+            if (moveLeft==true && player.Left>0)
+            {
+                player.Left -=speed;//sang trái thì trừ đi tốc độ
+            }
+            if (moveRight==true&&player.Left<677)
+            {
+                player.Left += speed;//sang phải thì cộng thêm tốc độ
+            }
+            if (moveUp == true && player.Top > 0)
+            {
+                player.Top -=speed;//lên trên thì trừ đi tốc độ
+            }
+            if (moveDown == true && player.Top < 334 )
+            {
+                player.Top +=speed;//xuống dưới thì cộng thêm tốc độ
+            }
+
+            //xử lý va chạm
+            foreach (Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag=="object")
+                {
+                    if(player.Bounds.IntersectsWith(x.Bounds))//nếu có va chạm
+                    {
+                        x.BackColor= Color.White;
+                        label1.Text = x.Name;
+                    }
+                    else
+                    {
+                        x.BackColor= Color.Yellow;
+                    }
+                }
+            }
+
+        }
+
+        //khi ta nhấn 1 phím xuống (chưa nhả phím ra)
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                moveLeft= true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                moveRight= true;
+            }
+            if(e.KeyCode == Keys.Up)
+            {
+                moveUp= true;
+            }
+            if(e.KeyCode==Keys.Down) 
+            {
+                moveDown= true;
+            }
+            
+        }
+
+        //khi ta nhả phím ra (phải trả về trạng thái ban đầu cho phím)
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                moveUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                moveDown = false;
+            }
+        }
+    }
+}
